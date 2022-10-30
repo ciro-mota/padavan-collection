e94de5db8e3557b0140c34789009c108cc71cdb5
Ciro Mota <ciro.mota@gmail.com>
2022-10-30 16:16:45 -0300

    fix: Update DDNS method

--- codes/ca-cert/update-cert.sh
+++ codes/ca-cert/update-cert.sh
@@ -1,33 +1,29 @@
 #!/bin/sh
 
 ## Variables
-ddns="your-domain.duckdns.org"
+sddns="your-site-name-duckdns" # Only sitename, without domain.
+ddns="your-site.duckdns.org" # Without https://.
 token="your-token-here"
 pathcrt="/home/admin/.acme.sh"
 
-## Export Token
-export DuckDNS_Token="$token"
-
 ## Update DDNS
-inadyn -a $ddns -u $token --ssl -S default@duckdns.org -i ppp0 -f 0 -V 1 -b -e /sbin/ddns_updated
-sleep 10
-kill $(pgrep inadyn) 2> /dev/null
+/opt/bin/wget --no-check-certificate -O - -o /opt/home/admin/duck.log "https://www.duckdns.org/update?domains=$sddns&token=$token&ip="
 
 ## Download ACME
-curl -fsSL https://get.acme.sh | sh &> /dev/null
+/opt/bin/curl -fsSL https://get.acme.sh | sh 2>&1 /dev/null
 
 ## Cert Generate
-/home/admin/.acme.sh/acme.sh --insecure --uninstall-cronjob --server letsencrypt --issue --dns dns_duckdns -d $ddns 2> /dev/null
+/opt/home/admin/.acme.sh/acme.sh --insecure --uninstall-cronjob --server letsencrypt --issue --dns dns_duckdns -d $ddns 2> /dev/null
 
-## Copy cert files
+## Copy Cert Files
 cp $pathcrt/$ddns/$ddns.key /etc/storage/https/server.key
 cp $pathcrt/$ddns/$ddns.cer /etc/storage/https/server.crt
 cp $pathcrt/$ddns/fullchain.cer /etc/storage/https/ca.crt
 
-## Restart httpd service
-kill $(pgrep httpd) 2> /dev/null
+## Restart httpd Service
+kill "$(pgrep httpd)" 2> /dev/null
 
-## Save configs
+## Save Configs
 mtd_storage.sh save
 
 ## Write results on the log.
