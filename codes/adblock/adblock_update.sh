c03bbe32248c9962924ed5c10badd26b5c40a548
Ciro Mota <ciro.mota@gmail.com>
2022-11-04 17:06:54 -0300

    fix: Changes in list address, added write log event and commit changes

--- codes/adblock/adblock_update.sh
+++ codes/adblock/adblock_update.sh
@@ -2,7 +2,7 @@
 PATH=/opt/sbin:/opt/bin:/opt/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 
-# 0.0.0.0 It is defined as a non-routable address used to designate an invalid, unknown, or inapplicable destination. In addition to being faster empirically, possibly because there is no waiting for a timeout resolution.
+## 0.0.0.0 It is defined as a non-routable address used to designate an invalid, unknown, or inapplicable destination. In addition to being faster empirically, possibly because there is no waiting for a timeout resolution.
 ENDPOINT_IP4="0.0.0.0"
 ENDPOINT_IP6="::"
 TMPDIR="/tmp/block.build.list"
@@ -11,32 +11,37 @@ TARGET="/tmp/block.hosts"
 DLIST="/etc/storage/adblock_deny.list"
 ALIST="/etc/storage/adblock_allow.list"
 
-# Download your block lists. They should be used in hosts format. Use wisely, not always many lists will yield a better result:
-wget -U "Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0" -qO- "https://block.energized.pro/spark/formats/hosts.txt" | awk -vr="$ENDPOINT_IP4" '{sub(/^0.0.0.0/, r)} $0 ~ "^"r' >> "$TMPDIR"
-wget -U "Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0" -qO- "https://block.energized.pro/spark/formats/hosts-ipv6.txt" | awk -vr="$ENDPOINT_IP6" '{sub(/^::/, r)} $0 ~ "^"r' >> "$TMPDIR"
+## Download your block lists. They should be used in hosts format. Use wisely, not always many lists will yield a better result:
+wget -qO- "https://block.energized.pro/spark/formats/hosts.txt" | awk -vr="$ENDPOINT_IP4" '{sub(/^0.0.0.0/, r)} $0 ~ "^"r' >> "$TMPDIR"
+wget -qO- "https://block.energized.pro/spark/formats/hosts-ipv6.txt" | awk -vr="$ENDPOINT_IP6" '{sub(/^::/, r)} $0 ~ "^"r' >> "$TMPDIR"
 
 
-# Add blacklist, if non-empty
+## Add blacklist, if non-empty
 if [ -s "$DLIST" ]
 then
 awk -v r="$ENDPOINT_IP4" '/^[^#]/ { print r,$1 }' "$DLIST" >> "$TMPDIR"
 fi
 
-# Sort the download/black lists
+## Sort the download/black lists
 awk '{sub(/\r$/,"");print $1,$2}' "$TMPDIR" | sort -u > "$STGDIR"
 
-# Filter (if applicable)
+## Filter (if applicable)
 if [ -s "$ALIST" ]
 then
-# Filter the blacklist, suppressing whitelist matches
-# This is relatively slow
+## Filter the blacklist, suppressing whitelist matches. This is relatively slow
 grep -E -v "^[[:space:]]*$" "$ALIST" | awk '/^[^#]/ {sub(/\r$/,"");print $1}' | grep -vf - "$STGDIR" > "$TARGET"
 else
 cat "$STGDIR" > "$TARGET"
 fi
 
-# Delete files used to build list to free up the limited space
+## Delete files used to build list to free up the limited space
 rm -f "$TMPDIR"
 rm -f "$STGDIR"
 
 killall -SIGHUP dnsmasq
+
+## Save Configs
+mtd_storage.sh save
+
+## Write results on the log.
+logger -t rstats "Lists has updated."
